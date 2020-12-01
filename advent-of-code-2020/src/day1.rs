a285c0368e6795f3fc02080d84d9a500d7def4e4
Matt Ellis <m.t.ellis@gmail.com>
2020-12-01 23:56:29 +0000

    Introduce generator

--- advent-of-code-2020/src/day1.rs
+++ advent-of-code-2020/src/day1.rs
@@ -1,9 +1,12 @@
-use aoc_runner_derive::{aoc};
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day1)]
+pub fn parse_input(input: &str) -> Vec<i32> {
+    return input.lines().map(|s| s.parse().unwrap()).collect();
+}
 
 #[aoc(day1, part1)]
-pub fn part1(input: &str) -> i32 {
-    let numbers = input.lines().map(|s| s.parse().unwrap()).collect::<Vec<i32>>();
-
+pub fn part1(numbers: &Vec<i32>) -> i32 {
     for num1 in numbers.iter() {
         for num2 in numbers.iter() {
             if num1 + num2 == 2020 {
@@ -16,9 +19,7 @@ pub fn part1(input: &str) -> i32 {
 }
 
 #[aoc(day1, part2)]
-pub fn part2(input: &str) -> i32 {
-    let numbers = input.lines().map(|s| s.parse().unwrap()).collect::<Vec<i32>>();
-
+pub fn part2(numbers: &Vec<i32>) -> i32 {
     for num1 in numbers.iter() {
         for num2 in numbers.iter() {
             for num3 in numbers.iter() {
